6d63749c839bd4ac46a9e911fe05dc8fcb77eb4f
DurvAcee <durvesh.danve@gmail.com>
2023-01-02 15:23:42 +0530

    added loop for showing news

--- src/Components/News.js
+++ src/Components/News.js
@@ -57,16 +57,13 @@ export class News extends Component {
     return (
       <div className = "container my-3">
         <h2>NewsMonkey - Top Headlines!</h2>
+
             <div className = "row">
-                <div className="col-md-4">
-                    <NewsItem title="myTitle" description="myDesc" imageUrl="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" newsUrl="TODO"/>
-                </div>
-                <div className="col-md-4">
-                    <NewsItem title="myTitle" description="myDesc"/>
-                </div>
-                <div className="col-md-4">
-                    <NewsItem title="myTitle" description="myDesc"/>
-                </div>
+                {this.state.articles.map((element)=>{
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                        </div>
+                })}
             </div>
       </div>
     )
